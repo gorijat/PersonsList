9c47c36449e108d1f584a9312900acde1f66cab5
Goran Gajic <you@example.com>
2023-01-14 17:27:01 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PersonsList from "./components/PersonsList";
+import NewPerson from "./components/NewPerson";
+//styles
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "Goran",
+    data : [
+      
+  ]
+}
+componentDidMount(){
+  let data = [];
+  if (localStorage.data) {
+    data = JSON.parse(localStorage.data)
+  }
+  this.setState({
+    data : data
+  })
 }
 
-export default App;
+insertNewPerson = (person)=>{
+  person.id = Math.floor(Math.random()*(10000-10)+10);
+  localStorage.data = JSON.stringify([...this.state.data,person]);
+  //let dataCopy = this.state.data.concat(person)
+  let dataCopy = [...this.state.data,person];
+  this.setState({
+    data : dataCopy
+  })
+}
+
+deletePerson = (id)=>{
+  
+  let filteredData = this.state.data.filter(el=>{
+    return el.id !== id;
+  })
+  localStorage.data = JSON.stringify(filteredData);
+
+  this.setState({
+    data : filteredData
+  })
+  
+  
+}
+
+    render(){
+      return(
+      <div>
+        <PersonsList deletePerson={this.deletePerson} data={this.state.data} />
+        <NewPerson insertNewPerson={this.insertNewPerson}/>
+      </div>
+      
+      )
+    }
+  }
+
+  export default App;
